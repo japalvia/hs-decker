e1542f0678a2f25c97e53205b31a10bbb9f7f89a
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2017-11-18 15:52:34 +0200

    Fix style, add more argument validation.

--- src/hscollection.py
+++ src/hscollection.py
@@ -31,17 +31,17 @@ class HSCollection:
             f = open(collection)
             self.mycollection = json.load(f)
         except FileNotFoundError:
-            print("Initializing empty collection")
+            print('Initializing empty collection')
             self.mycollection = json.loads('[]')
 
     def add_card(self, name, count):
         count = int(count)
         if count != 1 and count != 2:
-            self.error("Card count ({}) must be (1) or (2)".format(count))
+            self.error('Card count ({}) must be (1) or (2)'.format(count))
 
         card = self.card2json(name)
         if not card:
-            self.error("Card not found: {}".format(name))
+            self.error('Card not found: {}'.format(name))
         card['count'] = count
 
         new_card = True
@@ -51,10 +51,10 @@ class HSCollection:
                 if c['count'] == 1:
                     c['count'] = 2
                 elif c['count'] == 2:
-                    print("Already in collection: {} (2)".format(c['name']))
+                    print('Already in collection: {} (2)'.format(c['name']))
                     return
                 else:
-                    self.error("Card {} count is unexpected: {}"
+                    self.error('Card {} count is unexpected: {}'
                           .format(c['name'], c['count']))
                 break
         if new_card:
@@ -68,28 +68,28 @@ class HSCollection:
         with open(path) as f:
             for line in f:
                 line = line.strip()
-                # sys.stdout.write("process line: {}".format(line))
+                # sys.stdout.write('process line: {}'.format(line))
                 if not line:
-                    print("Skip empty line")
+                    print('Skip empty line')
                     continue
                 parts = line.split('#', 1)
                 if len(parts[0]) == 0: # line starts with comment
-                    print("comment line: {}".format(parts[1]))
+                    print('comment line: {}'.format(parts[1]))
                     continue
                 if len(parts) > 1:
-                    print("data: [{}] comment: [{}]".
+                    print('data: [{}] comment: [{}]'.
                           format(parts[0], parts[1]))
                 data = parts[0].split(' ', 1)
                 self.add_card(data[1].strip(), data[0])
 
 def usage():
-    print("Usage: {} <card name> <count>".format(sys.argv[0]), file=sys.stderr)
+    print('Usage: {} <card name> <count>'.format(sys.argv[0]), file=sys.stderr)
 
 def bad_usage(msg):
     usage()
     sys.exit(msg)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser('Manage Hearthstone cards collection')
     parser.add_argument('-c', '--collectible', help='cards.collectible.json')
     parser.add_argument('-m', '--mycollection', help='mycollection.json')
@@ -102,7 +102,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     if (args.card and not args.count) or (not args.card and args.count):
-        bad_usage("--card and --count must be used together")
+        bad_usage('--card and --count must be used together')
 
     collection = HSCollection(args.collectible, args.mycollection)
 
@@ -110,6 +110,8 @@ if __name__ == "__main__":
         collection.add_card(args.card, args.count)
     elif args.list:
         collection.add_from_file(args.list)
+    else:
+        bad_usage('--list or --card is required')
 
     collection.save()
 
