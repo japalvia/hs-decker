4a81aaf3c211200f5eeee4f2e66c54d7a1ad4a71
Jarkko Palviainen <jarkko.palviainen@gmail.com>
2017-11-18 22:46:06 +0200

    Added bootstrap command, updated documentation.

---
+++ src/bootstrap.py
@@ -0,0 +1,12 @@
+#!/bin/env python
+
+import urllib.request
+import shutil
+
+url = 'https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json'
+filename = 'data/cards.collectible.json'
+
+req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+response = urllib.request.urlopen(req)
+with open(filename, 'wb') as f:
+    shutil.copyfileobj(response, f)
